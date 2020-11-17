0e166a14ebca00df50c3f4c8eca00ad74bead64a
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-11-17 17:59:05 +0900

    clone init

---
+++ 01/js/ex.js
@@ -0,0 +1,22 @@
+// variable=변수== var
+var a="hello world";
+var b= 56;
+var c= 0.134237;
+//data type
+//기계어는 10101010111100, 
+//자바스크립트는 똑똑해 왜?
+// 무슨타입인지 잘 알아. 
+// 함수... 즐거운 함수 ...
+// y=2x
+// f(x)=2x
+// x의 값이 3일때
+// y값을 구하시오. 
+// y=? y=6
+
+var d = 2*b;
+//d=? 
+console.log("동재가 계산한 값은...", d);
+console.log("변수 d의 데이터 타입은? "+ typeof(d));
+
+var e = "동재 하이";
+console.log(e);

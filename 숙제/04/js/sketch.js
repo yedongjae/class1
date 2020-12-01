d45a94849c1427532f0b96aad7f6cb4b4bd16d57
dongjae ye <dongjaeye@gmail.com>
2020-12-01 16:08:26 +0900

    dddd

--- 숙제/04/js/sketch.js
+++ 숙제/04/js/sketch.js
@@ -4,3 +4,34 @@ console.log("주사위 눈의 갯수 ", dice);
 
 //반복문 이용해서 몇번 던지고, 
 //던질때마다 어떠한 반응!
+
+thr=Math.random()*10;
+console.log(thr);
+dongjae="앞면";
+djgf="뒷면";
+
+for( var i=0; i<2;i++){
+    if(thr>4){
+        coin="앞면";
+    }
+    else if(thr<4){
+        coin="뒷면";
+    }
+    else{
+        coin="앞면도 뒷면도 아냐"
+    }
+    console.log(coin);
+    
+    if(coin==dongjae){------
+        console.log("내가 이김 후후훗");
+    }
+    // else if(coin!="앞면입니다~"){-
+    //     console.log("에이 ㅅㅂ");
+    // } 결국 똑같다
+    else{
+        console.log("에이 ㅅㅂ");
+    }
+}
+
+var dice=Math.ceil(Math.random()*6);
+console.log("주사위 눈의 갯수 ", dice);
--- 예제/04/js/ex.js
+++ 예제/04/js/ex.js
@@ -32,3 +32,35 @@ for( var i=0; i<2;i++){
 }
 
 
+<<<<<<< Updated upstream
+=======
+//주사위 
+var dice=Math.ceil(Math.random()*6);
+console.log("주사위 눈의 갯수 ", dice);
+
+// 동전 던지기 generator
+
+var coin;// "앞면" "뒷면"나타내려고~ 
+var thr;// 계산용
+
+thr=Math.random()*10;
+console.log(thr);
+dongjae="앞면입니다~";
+djgf="뒷면입니다~";
+
+for( var i=0; i<2;i++){
+    if(thr>4){
+        coin="앞면입니다~";
+    }
+    else if(thr<4){
+        coin="뒷면입니다~";
+    }
+    else{
+        coin="동전이 세워졌습니다~"
+    }
+    console.log(coin);
+    
+    if(coin==dongjae){
+        console.log("내가 이김 후후훗");
+    }
+>>>>>>> Stashed changes

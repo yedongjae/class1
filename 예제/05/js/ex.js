14c0c73a66b51f58d903e892dc8cb42562cb0817
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-12-02 14:38:00 +0900

    ddd

---
+++ 예제/05/js/ex.js
@@ -0,0 +1,88 @@
+//하루 24시간동한 매 시간마다 친구가 찾아온대 누가 올지는 몰라
+//나는 오늘 철수랑 영미가 내앞에 올때마다 동전을 던질꺼야.
+//다른애가오면 연필을 던질꺼야 
+//나는 결과를 기록하겠어. 앞 뒤 다 셀꺼야
+//영미, 슬기, 아이린, 헤진, 철수, 준원
+var d; //null
+var sign; //앞 또는 뒤 (line 57-77 참조)
+var random;//누가올지모르는 친구를 정해주는 랜덤변수 (line 15 참조)
+var friend=["영미", "슬기", "아이린", "헤진", "철수", "준원"];
+var visitor;//방문한 친구의 이름
+var resultf=0;//던져서 앞이 나온 횟수
+var resultb=0;//던져서 뒤가 나온 횟수
+
+for(var i=0;i<23;i++){
+    var rand=Math.floor(Math.random()*6);//0,1,2,3,4,5
+    if(rand==0){
+        visitor=friend[0];//vistor변수의 값은 friend 변수 배열중 0번째값인 "영미"
+    }
+    else if(rand==1){
+        visitor=friend[1];
+    }
+    else if(rand==2){
+        visitor=friend[2];
+    }
+    else if(rand==3){
+        visitor=friend[3];
+    }
+    else if(rand==4){
+        visitor=friend[4];
+    }
+    else if(rand==5){
+        visitor=friend[5];
+    }
+    else{
+        visitor=friend[6];
+    }
+
+
+    if(visitor=="철수"){
+        all();
+    }
+    else if(visitor=="영미"){
+        all();
+    }
+    else{
+        console.log("연필어겅");
+    }
+    
+}
+console.log("앞: ", resultf, "뒤: ", resultb);
+
+function all(){
+    toss();//던지고
+    real(sign);//기록하고
+}
+
+function toss(){
+    d=Math.random()*2;
+    if(d>1){
+        sign="front";
+    }
+    else if(d<1){
+        sign="back";
+    }
+    else{
+        sign="standing";
+    }
+}
+
+function resultCheck(){
+    if(sign=="front"){
+        resultf++;
+    }
+    else if(sign=="back"){
+        resultb++;
+    }
+}
+
+function real(a){
+    if(a=="front"){
+        resultf++;
+    }
+    else if(a=="back"){
+        resultb++;
+    }
+}
+
+real(sign);

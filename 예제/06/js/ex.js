a60817a6424ba999c2a45e8d93c4ce0f5836765a
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-12-08 15:43:55 +0900

    Update ex.js

--- 예제/06/js/ex.js
+++ 예제/06/js/ex.js
@@ -18,36 +18,3 @@ var e=f.toFixed(2);
 console.log(e);
 
 
-// toFixed()
-// toString()
-// length()
-// valueOf()
-// replace()
-// split()
-// toLowerCase()
-// toUpperCase()
-// push()
-// pop()
-// ceil()
-// floor()
-// max()
-// min()
-// indexOf()
-// innerHTML()
-// var array = [1,2,3,4,5];
-// console.log(Math.max(a,f,e,456,789))
-
-
-
-
-// 선택, 아니라 해 
-// document.getElementById().innerHTML() 를 사용, 
-// 
-var ex = document.getElementById("example");
-console.log(ex);
-ex.innerHTML="hello world";
-
-//1. apple 이라는 단어가 5번이상 나오는 문단을 쓰세요. 
-//2. 그 문단 속에서 apple 을 찾아서 orange로 바꾸세요. (단어 사이엔 " "가있음.)
-//3. 바꾼후 그 문단을 HTML 의 id: example 인곳에 대입하여 나타내세요. 
-// hint: indexOf() 
